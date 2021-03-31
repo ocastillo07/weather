b3be7fcd120b756aa3cc4e1c843094f8851c848e
Oscar <oscarcastillog7@gmai.com>
2021-03-30 19:50:32 -0700

    JWT & Warden

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+  	namespace :v1 do
+      post :auth, to: "authentication#create"
+	  	resources :locations do
+	  		resources :recordings
+	  	end
+	  end
+  end
+
+end
